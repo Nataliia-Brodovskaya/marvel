219e1d88f2a602691c8f8130c48e80f72c6aaebb
Nataliia <nataliia.brodovskaya@gmail.com>
2023-06-16 22:43:18 +0200

    first ommit

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,34 @@
+import {useState, useCallback} from 'react';
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, header = {'Content-Type': 'application/json'}) => {
+        setLoading(true);
+
+    try { 
+        const responce = await fetch(url, {method, body, header}); 
+        
+        if(!responce.ok) {
+            throw new Error (`Couldn't fetch${url}, status:${responce.status}`);
+        }   
+
+        const data = await responce.json();
+
+        setLoading(false);
+        return data;
+
+    } catch(e) {
+        setLoading(false);
+        setError(e.message);
+        throw e;
+
+    }
+    }, []);
+
+    const clearError = useCallback(() =>setError(null), []);
+
+    return {loading, request, error, clearError}
+    
+}
